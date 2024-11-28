331ba467d6b3b3eb68ac9894f191eaab38af951e
Yash <sahib1701@gmail.com>
2024-11-28 13:01:27 -0500

    Final version of Scriptorium

--- scriptorium/pages/blogs/index.tsx
+++ scriptorium/pages/blogs/index.tsx
@@ -128,139 +128,149 @@ const Blogs = () => {
   }, [searchParams]);
 
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-800 dark:text-white">
-      <Navbar />
-      <div className="flex items-center justify-between max-w-4xl mx-auto mt-4 mb-4">
-        <button
-          onClick={() => (window.location.href = "/blogs/create")}
-          className="py-2 px-4 bg-blue-600 text-white rounded font-medium hover:bg-blue-700 transition text-sm sm:text-base sm:py-2 sm:px-4 md:text-base md:py-3 md:px-6"
-        >
-          Create New Blog
-        </button>
-        <h1 className="pr-44 text-3xl font-bold text-blue-600 flex-1 text-center">
-          Search Blogs
-        </h1>
-      </div>
+      <div
+        className={`min-h-screen flex flex-col bg-gray-100 dark:bg-gray-800 dark:text-white`}
+      >
+        <Navbar />
 
-      <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-6 dark:bg-gray-600 dark:text-white">
-        {/* Search Form */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-          <input
-            type="text"
-            name="title"
-            value={searchParams.title}
-            onChange={handleInputChange}
-            placeholder="Blog Title"
-            className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
-          />
-          <input
-            type="text"
-            name="description"
-            value={searchParams.description}
-            onChange={handleInputChange}
-            placeholder="Blog Description"
-            className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
-          />
-          <input
-            type="text"
-            name="tags"
-            value={searchParams.tags}
-            onChange={handleInputChange}
-            placeholder="Blog Tags (comma-separated)"
-            className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
-          />
-          <input
-            type="text"
-            name="templateIds"
-            value={searchParams.templateIds}
-            onChange={handleInputChange}
-            placeholder="Template IDs"
-            className="search-input md:col-span-3 rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
-          />
-          <div className="flex flex-col md:col-span-3">
-            <label className="text-black mb-2 font-medium dark:text-white">
-              Order By:
-            </label>
-            <select
-              name="order"
-              value={searchParams.order}
-              onChange={handleInputChange}
-              className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
+        {/* Main Content */}
+        <div className="flex-1">
+          <div className="flex items-center justify-between max-w-4xl mx-auto mt-4 mb-4">
+            <button
+              onClick={() => (window.location.href = "/blogs/create")}
+              className="py-2 px-4 bg-blue-600 text-white rounded font-medium hover:bg-blue-700 transition text-sm sm:text-base sm:py-2 sm:px-4 md:text-base md:py-3 md:px-6"
             >
-              <option value="">Newest</option>
-              <option value="upvotes">Upvotes</option>
-              <option value="downvotes">Downvotes</option>
-              {userRole === "ADMIN" && (
-                <option value="reports">Reports</option>
-              )}
-            </select>
+              Create New Blog
+            </button>
+            <h1 className="pr-44 text-3xl font-bold text-blue-600 flex-1 text-center">
+              Search Blogs
+            </h1>
+          </div>
+
+          <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-6 dark:bg-gray-600 dark:text-white">
+            {/* Search Form */}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+              <input
+                type="text"
+                name="title"
+                value={searchParams.title}
+                onChange={handleInputChange}
+                placeholder="Blog Title"
+                className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
+              />
+              <input
+                type="text"
+                name="description"
+                value={searchParams.description}
+                onChange={handleInputChange}
+                placeholder="Blog Description"
+                className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
+              />
+              <input
+                type="text"
+                name="tags"
+                value={searchParams.tags}
+                onChange={handleInputChange}
+                placeholder="Blog Tags (comma-separated)"
+                className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
+              />
+              <input
+                type="text"
+                name="templateIds"
+                value={searchParams.templateIds}
+                onChange={handleInputChange}
+                placeholder="Template IDs"
+                className="search-input md:col-span-3 rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
+              />
+              <div className="flex flex-col md:col-span-3">
+                <label className="text-black mb-2 font-medium dark:text-white">
+                  Order By:
+                </label>
+                <select
+                  name="order"
+                  value={searchParams.order}
+                  onChange={handleInputChange}
+                  className="search-input rounded bg-gray-100 dark:bg-gray-900 dark:text-white"
+                >
+                  <option value="">Newest</option>
+                  <option value="upvotes">Upvotes</option>
+                  <option value="downvotes">Downvotes</option>
+                  {userRole === "ADMIN" && (
+                    <option value="reports">Reports</option>
+                  )}
+                </select>
+              </div>
+            </div>
+
+            {error && <p className="text-red-500 mt-4">{error}</p>}
+          </div>
+
+          {/* Blog Posts */}
+          <div className="max-w-4xl mx-auto mt-8">
+            {blogs.length > 0 ? (
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {blogs.map((blog: any) => (
+                  <button
+                    key={blog.id}
+                    className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg dark:bg-gray-600 dark:text-white hover:scale-105 hover:shadow-blue-400/50 transition duration-300 ease-in-out"
+                    onClick={() => (window.location.href = `/blogs/${blog.id}`)}
+                  >
+                    <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200 break-words">
+                      {blog.title}
+                    </h2>
+                    <p className="text-gray-600 mt-2 dark:text-white break-words">
+                      {blog.description}
+                    </p>
+                    <div className="flex items-center justify-between text-sm text-gray-500 mt-4 dark:text-gray-400">
+                      <span className="text-black dark:text-white">
+                        Upvotes: {blog.upvotes}
+                      </span>
+                      <span className="text-black dark:text-white">
+                        Downvotes: {blog.downvotes}
+                      </span>
+                    </div>
+
+                    {/* Display reports count for admins */}
+                    {userRole === "ADMIN" && (
+                      <div className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                        Reports: {blog.reports}
+                      </div>
+                    )}
+
+                    <div className="text-sm text-gray-400 mt-2 dark:text-gray-200">
+                      Tags:
+                      {blog.tags.split(",").map((tag, index) => (
+                        <span
+                          key={index}
+                          className="px-2 py-0.5 text-xs bg-gray-100 text-gray-800 rounded-full m-0.5"
+                        >
+                          {tag.trim()}
+                        </span>
+                      ))}
+                    </div>
+                  </button>
+                ))}
+              </div>
+            ) : (
+              <p className="text-red-600 text-center">No blogs found.</p>
+            )}
           </div>
         </div>
 
-        {error && <p className="text-red-500 mt-4">{error}</p>}
-      </div>
-
-      {/* Blog Posts */}
-      <div className="max-w-4xl mx-auto mt-8">
-        {blogs.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {blogs.map((blog: any) => (
-              <button
-                key={blog.id}
-                className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg dark:bg-gray-600 dark:text-white hover:scale-105 hover:shadow-blue-400/50 transition duration-300 ease-in-out"
-                onClick={() => (window.location.href = `/blogs/${blog.id}`)}
-              >
-                <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200 break-words">
-                  {blog.title}
-                </h2>
-                <p className="text-gray-600 mt-2 dark:text-white break-words">
-                  {blog.description}
-                </p>
-                <div className="flex items-center justify-between text-sm text-gray-500 mt-4 dark:text-gray-400">
-                  <span className="text-black dark:text-white">
-                    Upvotes: {blog.upvotes}
-                  </span>
-                  <span className="text-black dark:text-white">
-                    Downvotes: {blog.downvotes}
-                  </span>
-                </div>
-
-                {/* Display reports count for admins */}
-                {userRole === "ADMIN" && (
-                  <div className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                    Reports: {blog.reports}
-                  </div>
-                )}
-
-                <div className="text-sm text-gray-400 mt-2 dark:text-gray-200">
-                  Tags:
-                  {blog.tags.split(",").map((tag, index) => (
-                    <span
-                      key={index}
-                      className="px-2 py-0.5 text-xs bg-gray-100 text-gray-800 rounded-full m-0.5"
-                    >
-                      {tag.trim()}
-                    </span>
-                  ))}
-                </div>
-              </button>
-            ))}
+        {/* Pagination */}
+        {blogs.length > 0 && (
+          <div className="mb-4">
+            <Pagination
+              currentPage={searchParams.pageNum}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
+            />
           </div>
-        ) : (
-          <p className="text-red-600 text-center">No blogs found.</p>
         )}
-      </div>
 
-      {/* Pagination */}
-      <div className="mb-4">
-        <Pagination
-          currentPage={searchParams.pageNum}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
+        {/* Footer */}
+        <Footer />
       </div>
-      <Footer />
-    </div>
   );
 };
 
